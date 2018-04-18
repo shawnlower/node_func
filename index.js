86840d503d91b61cb440d8255783bd92d3a0b774
Shawn Lower <shawn@shawnlower.com>
2018-04-18 00:31:40 -0400

    Adding example method for gcs trigger

--- index.js
+++ index.js
@@ -3,3 +3,26 @@
 exports.indexGET = (req, res) => {
   res.status(200).send('Hello Node World');
 };
+
+/**
+ * Background Cloud Function to be triggered by Cloud Storage.
+ *
+ * @param {object} event The Cloud Functions event.
+ * @param {function} callback The callback function.
+ */
+exports.helloGCS = (event, callback) => {
+  const file = event.data;
+
+  if (file.resourceState === 'not_exists') {
+    console.log(`File ${file.name} deleted.`);
+  } else if (file.metageneration === '1') {
+    // metageneration attribute is updated on metadata changes.
+    // on create value is 1
+    console.log(`File ${file.name} uploaded.`);
+  } else {
+    console.log(`File ${file.name} metadata updated.`);
+  }
+
+  callback();
+};
+
