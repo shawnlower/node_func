e04a074cd781bfd35401913b3cb96f200e947d51
Shawn Lower <shawn@shawnlower.com>
2018-04-18 00:02:06 -0400

    rewrite for cloud funcs

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-const express = require('express');
-
-const app = express();
-
-app.get('/', function(req, res){
+exports.indexGET = (req, res) => {
   res.status(200).send('Hello Node World');
-});
-
-app.listen(3000, () => console.log("Listening on port 3000. Maybe."));
+};
